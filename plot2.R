7e8622e47b1ce9422d01205eb09fc8d814affe63
God Man <pneumatick@pop-os.localdomain>
2021-07-18 17:54:38 -0400

    Committer: pneumatick <pneumatick@pop-os.localdomain>  Changes to be committed: 	new file:   pics/plot2.png 	modified:   plot1.R 	new file:   plot2.R

--- plot2.R
+++ plot2.R
@@ -7,15 +7,17 @@ if (!file.exists("data/household_power_consumption.txt")) {
 
 powerData <- read.csv("data/household_power_consumption.txt", sep = ";")
 powerData <- powerData[powerData$Date == "1/2/2007" |  powerData$Date == "2/2/2007", ]
+powerData <- powerData[powerData$Global_active_power != "?", ]
 powerData$Global_active_power <- as.numeric(powerData$Global_active_power)
 
-# Create plot (histogram)
-hist(powerData$Global_active_power,
-     col = "red",
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)")
+# Create the plot
+date <- paste(powerData$Date, powerData$Time)
+time <- as.numeric(as.POSIXct(strptime(date, "%d/%m/%Y %H:%M:%S")))
+with(powerData, plot(time, Global_active_power, type = "l", xaxt = "n", xlab = "", ylab = "Global Active Power (kilowatts)"))
+axis(1, at = c(1170306000, 1170392370, 1170478740), labels = c("Thu", "Fri", "Sat"))
 par(bg = NA)
 
-# Export the plot as a PNG file
-dev.copy(png, file = "pics/plot1.png")
+# Export the plot as a PNG
+dev.copy(png, file = "pics/plot2.png")
 dev.off()
+
